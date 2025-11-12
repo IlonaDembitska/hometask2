40d07322a38654a1314d4b60bc3e90c42ede77da
Ilona Dembitska <144057082+IlonaDembitska@users.noreply.github.com>
2025-11-12 14:35:34 +0100

    utilities setup
    Removed user password setup and SSH configuration changes. Streamlined installation and setup of utilities and Apache service.

--- utilities-setup.sh
+++ utilities-setup.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
-echo "ec2-user:user" | sudo chpasswd
 
-# Дозволяємо вхід по паролю
-sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
-sudo systemctl restart sshd
+yum update -y
+yum install -y mc vim git httpd
 
-# Оновлюємо систему і встановлюємо утиліти
-sudo yum update -y
-sudo yum install -y mc vim git httpd
-
-# Запускаємо і вмикаємо Apache
-sudo systemctl start httpd
-sudo systemctl enable httpd
-
-# Створюємо тестову сторінку
-echo "<h1>Hello from AWS EC2</h1><p>mc, vim, git, apache are installed!</p>" | sudo tee /var/www/html/index.html
+systemctl start httpd
+systemctl enable httpd
